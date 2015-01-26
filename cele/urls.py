d53c9d4f133115de7f73cf2c7a95734bbb488afc
rayx2008 <rayx2008@gmail.com>
2015-01-26 08:58:02 +0800

    0126

--- cele/urls.py
+++ cele/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from polls import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls')),
+   
 )
--- polls/views.py
+++ polls/views.py
@@ -1,2 +1,18 @@
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+
+from polls.models import Question
+# ...
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+def index(request,question_id):
+    return render()
+
+def vote(request,question_id):
+    return render()
+
+def results(request,question_id):
+    return render()
+    
