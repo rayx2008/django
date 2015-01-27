18ba106c97fda1f85d154647ef2f50bcab26992c
rayx2008 <rayx2008@gmail.com>
2015-01-27 16:33:13 +0800

    add dirbot to learn

--- cele/urls.py
+++ cele/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from polls import views
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,5 +9,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls',namespace = "polls")),
+    
+    #spiderpro is my django spider program by ray
+    url(r'^spiderpro',include('spiderpro.urls',namespace = "spiderpro"))
    
 )
